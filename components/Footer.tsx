3e41ed96b8e84d6cc245f6b054e7d132d5fa0c71
raftkon <raftopouloskon@gmail.com>
2023-11-11 04:51:27 +0200

    style footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,10 +7,10 @@ export default function Footer() {
     <footer className="bg-darkBlue2 pt-12">
       <div className="container mx-auto px-5 pb-10">
         {/* Flex container for all items */}
-        <div className="flex flex-col justify-between space-y-24 md:flex-row md:space-y-0">
+        <div className="flex flex-col justify-between text-center space-y-24 md:flex-row md:space-y-0">
           {/* Email and phone */}
           <div className="mt-10 space-y-6">
-            <div className="flex items-center space-x-3 md:-mt-10">
+            <div className="flex items-center justify-center md:justify-start space-x-3 md:-mt-10">
               <div className="w-6">
                 <Image
                   src={"/images/icon-phone.svg"}
@@ -23,7 +23,7 @@ export default function Footer() {
               <div className="text-white ">+1-543-123-4567</div>
             </div>
 
-            <div className="flex items-center space-x-3 md:-mt-10">
+            <div className="flex items-center justify-center md:justify-start space-x-3 md:-mt-10">
               <div className="w-6">
                 <Image
                   src={"/images/icon-email.svg"}
@@ -40,25 +40,25 @@ export default function Footer() {
           <div className="flex text-white flex-col space-y-10 text-xl md:text-base md:space-x-20 md:space-y-0 md:flex-row">
             <div className="flex flex-col space-y-3">
               <Link
-                className="hover:border-b border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
+                className="hover:border-b mx-auto md:w-fit md:mx-0 w-2/3 border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
                 href={"#"}
               >
                 About
               </Link>
               <Link
-                className="hover:border-b border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
+                className="hover:border-b mx-auto md:w-fit md:mx-0 w-2/3 border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
                 href={"#"}
               >
                 Jobs
               </Link>
               <Link
-                className="hover:border-b border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
+                className="hover:border-b mx-auto md:w-fit md:mx-0 w-2/3 border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
                 href={"#"}
               >
                 Press
               </Link>
               <Link
-                className="hover:border-b border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
+                className="hover:border-b mx-auto md:w-fit md:mx-0 w-2/3 border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
                 href={"#"}
               >
                 Blog
@@ -66,19 +66,19 @@ export default function Footer() {
             </div>
             <div className="flex flex-col space-y-3">
               <Link
-                className="hover:border-b border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
+                className="hover:border-b mx-auto md:w-fit md:mx-0 w-2/3 border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
                 href={"#"}
               >
                 Contact Us
               </Link>
               <Link
-                className="hover:border-b border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
+                className="hover:border-b mx-auto md:w-fit md:mx-0 w-2/3 border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
                 href={"#"}
               >
                 Terms
               </Link>
               <Link
-                className="hover:border-b border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
+                className="hover:border-b mx-auto md:w-fit md:mx-0 w-2/3 border-b border-opacity-0 hover:border-opacity-100 border-gray-400 transition duration-300"
                 href={"#"}
               >
                 Privacy
